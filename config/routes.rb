f5218ee96f5a0763066bea4a9935126f38fe14a4
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-07-17 10:28:54 -0600

    added routes for chores teams tasks and friendships

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "static_pages#index"
   get 'user/:id' => 'users#show', as: :user_show
+  resources :teams, :only => [:new, :edit, :show, :destroy]
+  resources :friendships, :only => [:new, :edit, :show, :destroy]
+  resources :chores, :only => [:new, :edit, :show, :destroy]
+  resources :tasks, :only => [:new, :edit, :show, :destroy]
 end
