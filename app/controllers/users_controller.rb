b900220f3d7c00c1c539fdab3b0534d005a83cba
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-07-16 18:42:30 -0600

    Added user profile page action and route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,7 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+
+  def show
+    @user = User.find_by_id(params[:id])
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: "static_pages#index"
+  get 'user/:id' => 'users#show', as: :user_show
 end
