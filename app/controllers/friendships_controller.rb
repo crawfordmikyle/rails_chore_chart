7155e13069d5ee1835f41ec2f4a2f9aee36635d6
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-07-18 09:45:00 -0600

    friendship create

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,6 +2,7 @@ class FriendshipsController < ApplicationController
   before_action :authenticate_user!
 
   def create
-    
+    @friendship = current_user.friendships.build(:friend_id => params[:id])
+    binding.pry
   end
 end
