4d1158e10aee5b842c879a431c7ea9e532234bf9
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-07-17 19:09:13 -0600

    added tasks build from team page

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,3 +1,9 @@
 class TasksController < ApplicationController
   before_action :authenticate_user!
+
+  def create
+    Task.create({:chore_id => params[:id], :user_id => current_user.id})
+    redirect_to user_show_path(current_user)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "static_pages#index"
   get 'user/:id' => 'users#show', as: :user_show
-  resources :teams, :only => [:new, :edit, :show, :create, :destroy]
+  resources :teams, :only => [:index, :new, :edit, :show, :create, :destroy]
   resources :friendships, :only => [:new, :edit, :show, :create, :destroy]
   resources :chores, :only => [:new, :edit, :show, :create, :destroy]
   resources :tasks, :only => [:new, :edit, :show, :create, :destroy]
