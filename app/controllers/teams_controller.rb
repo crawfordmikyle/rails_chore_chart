0faecb2dc565271d90ff224c13857746d33443da
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-07-18 14:33:46 -0600

    working on friendship create

--- app/controllers/teams_controller.rb
+++ app/controllers/teams_controller.rb
@@ -29,11 +29,3 @@ private
     params.require(:team).permit(:name, :user_teams_attributes => {:user_id => []})
   end
 end
-
-#<!-- {"utf8"=>"✓", "authenticity_token"=>"tPDMtdb4c5o/zbyMJga9pz3yHAaSvNCZXSFUe44HvWtwyiCKS9baX87MC/f3lGKUfCClH5U3FrcBtZwje7Vj6Q==",
-#{}"team"=>{"name"=>"Test Team"},
-#{}"user_teams"=>["1", "2"],
-#{}"commit"=>"Create Team",
-#{}"controller"=>"teams",
-#{}"action"=>"create"}
-#permitted: false> -->
