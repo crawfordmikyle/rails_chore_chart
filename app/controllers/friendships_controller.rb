4ee1d1b068083de8977e53fe83101ae6a8213066
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-07-18 09:28:44 -0600

    working on friend create action

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,3 +1,7 @@
 class FriendshipsController < ApplicationController
   before_action :authenticate_user!
+
+  def create
+    
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,10 +2,11 @@ class UsersController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @users = User.all
   end
 
   def show
     @user = User.find_by_id(params[:id])
   end
-  
+
 end
