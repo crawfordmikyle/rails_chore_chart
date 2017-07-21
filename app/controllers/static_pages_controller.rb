5a16ab00746a1ec38fa15959f9a0b03fb9869eb6
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-07-21 15:11:59 -0600

    some form cleanup and added flash messages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,7 @@
 class StaticPagesController < ApplicationController
   def index
     if current_user
-      redirect_to user_show_path(current_user)
+      redirect_to user_path(current_user)
     else
       :index
     end
