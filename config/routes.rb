05efa87976e5c53c72a05e15e497fbc76486e787
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-07-21 15:33:03 -0600

    cleaned up some unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,10 +4,10 @@ Rails.application.routes.draw do
   get 'user/:id' => 'users#show', as: :user
   get 'users' => 'users#index', as: :users
   resources :teams, :only => [:index, :new, :edit, :show, :create, :destroy]
-  resources :friendships, :only => [:new, :edit, :show, :create, :update, :destroy]
+  resources :friendships, :only => [:create, :update, :destroy]
   resources :chores, :only => [:new, :edit, :show, :create, :destroy]
   resources :tasks, :only => [:new, :edit, :show, :create, :update, :destroy]
-  resources :friendship_requests
-  resources :user_team_requests
-  resources :user_teams
+  resources :friendship_requests, :only => [:create, :update, :destroy]
+  resources :user_team_requests, :only => [:create, :update, :destroy]
+  resources :user_teams, :only => [:create, :update, :destroy]
 end
