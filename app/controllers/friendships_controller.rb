a3838c7f3f44a0f64dc51855955e920c3acb4495
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-07-21 09:56:42 -0600

    working on the user friendships create

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -1,26 +1,4 @@
 class FriendshipsController < ApplicationController
   before_action :authenticate_user!
 
-  def create
-    @friendship = current_user.friendships.build(:friend_id => params[:friend_id])
-    if @friendship.save
-      flash[:notice] = "Added friend."
-      redirect_to root_url
-    else
-      flash[:error] = "Unable to add friend."
-      redirect_to root_url
-    end
-  end
-
-  #used to confirm friendships
-  def update
-    @friendship = current_user.inverse_friendships.find_by(:user_id => params[:id])
-    @friendship.status = "Accepted"
-    @friendship.save
-    redirect_to user_show_path(current_user.id)
-  end
-
-  def destroy
-
-  end
 end
--- app/models/friendship_request.rb
+++ app/models/friendship_request.rb
@@ -1,4 +1,9 @@
 class FriendshipRequest < ApplicationRecord
   belongs_to :user
   belongs_to :friend, :class_name => "User"
+
+  def create_friendship
+    binding.pry
+    current_user.friendships.build
+  end
 end
