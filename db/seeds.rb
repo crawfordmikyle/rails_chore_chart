6c807d0622d59521d566f716e5de9784f360d2b5
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-07-24 13:55:29 -0600

    started using user partials

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,34 +13,3 @@
   user_data.each do |user|
     User.create(user)
   end
-# Team
-  team_data = [
-    {:name => "Team One"},
-    {:name => "Team Two"},
-    {:name => "Team Three"},
-    {:name => "Team Four"}
-  ]
-
-  team_data.each do |team|
-    Team.create(team)
-  end
-# User Team
-
-# Chore
-  chore_data = [
-    {:name => "Clean Bathroom", :value => 10},
-    {:name => "Clean Bedroom", :value => 5},
-    {:name => "Take Out Trash", :value => 3},
-    {:name => "Walk Dog", :value => 1 },
-    {:name => "Cook Dinner", :value => 1 },
-    {:name => "Wash Laundry", :value => 3 }
-    ]
-
-    Team.all.each do |team|
-      chore_data.each do |chore|
-        team.chores.create(chore)
-      end
-    end
-# Task
-
-# Frindship
