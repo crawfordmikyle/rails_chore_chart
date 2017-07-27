cb8a3f576b13a380e77726e38bad07c71841843c
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-07-27 09:37:30 -0600

    working on leave team and unfriend

--- app/controllers/user_teams_controller.rb
+++ app/controllers/user_teams_controller.rb
@@ -1,6 +1,10 @@
 class UserTeamsController < ApplicationController
   before_action :authenticate_user!
 
+  def destroy
+    
+  end
+
 private
   def user_teams_params
     params.require(:user_teams_attributes).permit(:user_id)
