f58819bc0e29d1386ae3db8eaaa4250492d0b4a9
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-08-28 14:54:05 -0700

    More js for the chore task

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -1,18 +1,18 @@
 $(function() {
-    console.log( "ready!" );
-    loadEventListeners();
-});
-
-function loadEventListeners(){
 	$(".new_task").on("click",function(e){
 		e.preventDefault()
 		let postData = $(this).serialize();
-		debugger
+		$.post('/tasks', postData, function(responce){
+			debugger
+		})
 	})
 
 	$(".update_task").on("click",function(e){
 		e.preventDefault()
 		let postData = $(this).serialize();
+		let postRequest = $.post(url, data, success)
 		debugger
 	})
-}
+});
+
+
