e2fc9412b16e3eb2e2b8a23c3334297fccdf872f
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-08-28 13:00:34 -0700

    planning new ajax friendly forms for tasks

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -1,15 +1,18 @@
-function loadEventListeners(){
-	$(".accept-chore-btn").on("click",function(e){
-		e.preventDefault()
-		console.log(this)
-	})
-	$(".complete-chore-btn").on("click",function(e){
-		e.preventDefault()
-		console.log(this)
-	})
-}
-
 $(function() {
     console.log( "ready!" );
     loadEventListeners();
 });
+
+function loadEventListeners(){
+	$(".accept-chore-btn").on("click",function(e){
+		e.preventDefault()
+		debugger
+		console.log(this)
+	})
+
+	$(".complete-chore-btn").on("click",function(e){
+		e.preventDefault()
+		debugger
+		console.log(this)
+	})
+}
