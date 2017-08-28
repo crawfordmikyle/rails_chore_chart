df0ea06a411753b78830e4b02c7d8e1130614033
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-08-28 12:08:28 -0700

    added event for accept and complete btns

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -1,11 +1,15 @@
+function loadEventListeners(){
+	$(".accept-chore-btn").on("click",function(e){
+		e.preventDefault()
+		console.log(this)
+	})
+	$(".complete-chore-btn").on("click",function(e){
+		e.preventDefault()
+		console.log(this)
+	})
+}
+
 $(function() {
     console.log( "ready!" );
+    loadEventListeners();
 });
-
-function chore(){
-	
-}
-
-function loadChores (){
-
-}
