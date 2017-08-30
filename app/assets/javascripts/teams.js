172f47da0254c31e42b0b4287024d2d9d8206b49
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-08-30 10:08:44 -0700

    working on the post responce

--- app/assets/javascripts/teams.js
+++ app/assets/javascripts/teams.js
@@ -1,6 +1,7 @@
 $(function() {
-	$(".new_task").on("click",function(e){
-		e.preventDefault()
+	$(".new_task").on("submit",function(e){
+		e.preventDefault();
+		debugger
 		let postData = $(this).serialize();
 		$.post('/tasks', postData, function(responce){
 			debugger
@@ -9,13 +10,22 @@ $(function() {
 		})	
 	})
 
-	$(".edit_task").on("click",function(e){
-		e.preventDefault()
+	$(".edit_task").on("submit",function(e){
+		e.preventDefault();
+		debugger
 		let choreID = this.parentNode.id
 		let postData = $(this).serialize();
 		$.post('/tasks/'+choreID, postData, function(responce){
 			debugger
 			$("#"+choreID).append("<span class='label label-success'>COMPLETED</span>")
+		})
+	})
+
+	$(".new_chore").on("submit",function(e){
+		e.preventDefault();
+		let teamID = this.children.chore_team_id.value
+		let postData = $(this).serialize();
+		$.post("/teams/"+teamID+"/chores/new",postData,function(responce){
 		})
 	})
 })
