9951877c547115c09b0c2ec2b11d92e922e343e2
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-08-30 16:42:12 -0700

    last changed caused problems

--- app/assets/javascripts/teams/show.js
+++ app/assets/javascripts/teams/show.js
@@ -1,27 +1,12 @@
 $(function() {
 	$("input").removeAttr('data-disable-with');
-	reloadEventListeners();
-	$(".new_chore").on("submit",function(e){
-		e.preventDefault();
-		let teamID = this.children.chore_team_id.value
-		let postData = $(this).serialize();
-		$.post("/teams/"+teamID+"/chores/new",postData,function(responce){
-			$("#chores-div").append(responce);
-			reloadEventListeners();
-		})
-		this.reset();
-		$("input").removeAttr('data-disable-with');
-		$("#submit-new-chore").prop('disabled', false);
-	})
-})
-
-function reloadEventListeners(){
+	
 	$(".new_task").on("submit",function(e){
 		e.preventDefault();
 		let choreID = this.parentNode.id
 		let postData = $(this).serialize();
 		$.post('/tasks', postData, function(responce){
-			$("#"+choreID).replaceWith(responce).after(reloadEventListeners())
+			$("#"+choreID).replaceWith(responce)
 		})	
 	})
 
@@ -33,4 +18,17 @@ function reloadEventListeners(){
 			$("#"+choreID).replaceWith(responce);
 		})
 	})
-}
+
+	$(".new_chore").on("submit",function(e){
+		e.preventDefault();
+		let teamID = this.children.chore_team_id.value
+		let postData = $(this).serialize();
+		$.post("/teams/"+teamID+"/chores/new",postData,function(responce){
+			$("#chores-div").append(responce);
+		})
+		this.reset();
+		$("input").removeAttr('data-disable-with');
+		$("#submit-new-chore").prop('disabled', false);
+	})
+})
+
--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -13,7 +13,6 @@ class TasksController < ApplicationController
   #used to mark chore completed
   def update
     task = Task.find_by(chore_id: params[:id])
-    team = task.chore.team
     if task.user_id == current_user.id
       task.add_points_to_user
       task.chore.mark_completed
