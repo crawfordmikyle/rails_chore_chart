aa92257f7e52a09beea359c633b7b5ed647634b1
Mikyle Crawford <crawfordmikyle@gmail.com>
2017-08-31 11:10:51 -0700

    added handlebars back in render users index via json

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,6 @@
 //= require rails-ujs
 //= require jquery
 //= require jquery_ujs
+//= require handlebars
+//= require_tree ./templates
 //= require_tree .
--- app/assets/javascripts/users/index.js
+++ app/assets/javascripts/users/index.js
@@ -1,11 +1,15 @@
-var users = []
-
-function User(name,teams,points){
+function User(id,name,teams,points){
+	this.id = id
 	this.name = name
 	this.teams = teams
 	this.points = points
 }
 
+function liveSearch(){
+	let = input = $()
+}
+
+var users = []
 $(function() {
 	$.ajax({
         type: "GET",
@@ -14,15 +18,30 @@ $(function() {
         success: function(responce){
         	let userData = responce.data;
         	for(var i = userData.length - 1; i >= 0; i--) {
+        		let userID = userData[i].id
         		let userInfo = userData[i].attributes;
         		let userName = userInfo.name;
         		let userPoints = userInfo.points;
-        		let userTeams = userInfo.teams;
-        		users.push(new User(userName,userTeams,userPoints));
+        		let teamNames = []
+
+        		//if (userInfo.teams.length > 0){
+	        	//	for (var i = userInfo.teams.length - 1; i >= 0; i--) {
+	        	//		teamNames.push(userInfo.teams[i].name)
+	        	//	}
+        		//}
+
+        		users.push(new User(userID,userName,teamNames,userPoints));
+        	}
+        },
+        complete: function(){
+        	for (var i = users.length - 1; i >= 0; i--) {
+        		var source   = $("#user-template").html();
+						var template = Handlebars.compile(source);
+						$("#user-div").append (template(users[i]));
         	}
         }
-    });
+    })
 
-     
+
 });
 
